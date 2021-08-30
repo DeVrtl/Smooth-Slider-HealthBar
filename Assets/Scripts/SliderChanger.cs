dcdc608505e4a7ddb939f2b62e7b451ee61df2f1
DeVrtl <88022703+DeVrtl@users.noreply.github.com>
2021-08-30 18:45:51 +0500

    init

---
+++ Assets/Scripts/SliderChanger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderChanger : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private float _speed;
+
+    private float _currentProgress;
+
+    private void Update()
+    {
+        HandleBar();
+    }
+
+    public void IncreaseSlider()
+    {
+        _currentProgress += 10;
+        HandleBar();
+    }
+
+    public void DecreaseSlider()
+    {
+        _currentProgress -= 10;
+        HandleBar();
+    }
+
+    public void HandleBar()
+    {
+        _slider.value = Mathf.MoveTowards(_slider.value, _currentProgress, _speed * Time.deltaTime);
+    }
+}
