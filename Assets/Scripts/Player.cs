140f7fc0c49c1c272a055e27f5affbb31f5fe587
DeVrtl <88022703+DeVrtl@users.noreply.github.com>
2021-09-02 21:20:07 +0500

    Code Improves

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,39 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _maxHealth = 100f;
-    [SerializeField] private SliderChanger _slider;
-
-    private float _currentHealth;
+    private float _maxHealth = 100f;
     private float _templateNumber = 10;
+    private float _currentHealth;
 
-    private void Start()
-    {
-        _currentHealth = _maxHealth;
-        _slider.SetMaxProgress(_maxHealth);
-    }
-
-    private void Update()
-    {
-        _slider.SetProgress(_currentHealth);
-    }
+    public float MaxHealth => _maxHealth;
 
     public void TakeDamage()
     {
         _currentHealth -= _templateNumber;
-
-        SettingProgress(_currentHealth);
+        _maxHealth = _currentHealth;
     }
 
-    public void AddHealth()
+    public void Heal()
     {
         _currentHealth += _templateNumber;
-
-        SettingProgress(_currentHealth);
-    }
-
-    private void SettingProgress(float number)
-    {
-        _slider.SetProgress(number);
+        _maxHealth = _currentHealth;
     }
 }
--- Assets/Scripts/SliderChanger.cs
+++ Assets/Scripts/SliderChanger.cs
@@ -8,12 +8,6 @@ public class SliderChanger : MonoBehaviour
     [SerializeField] private Slider _slider;
     [SerializeField] private float _speed;
 
-    public void SetMaxProgress(float progress)
-    {
-        _slider.maxValue = progress;
-        _slider.value = progress;
-    }
-
     public void SetProgress(float progress)
     {
         _slider.value = Mathf.MoveTowards(_slider.value, progress, _speed * Time.deltaTime);
